4891f088b9db1d5c770dbe224df05d82451d758d
Hannes Ebner <hannes@metasolutions.se>
2014-05-13 21:03:51 +0200

    refactored methods into cacheimpl

--- webapp/src/main/java/org/entrystore/ldcache/resources/CacheResource.java
+++ webapp/src/main/java/org/entrystore/ldcache/resources/CacheResource.java
@@ -17,19 +17,14 @@
 package org.entrystore.ldcache.resources;
 
 import org.apache.log4j.Logger;
-import org.entrystore.ldcache.cache.Resource;
-import org.entrystore.ldcache.cache.impl.RdfResource;
-import org.entrystore.ldcache.util.HttpUtil;
+import org.entrystore.ldcache.cache.Cache;
+import org.entrystore.ldcache.util.JsonUtil;
 import org.entrystore.ldcache.util.RdfMedia;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.openrdf.model.Model;
 import org.openrdf.model.URI;
 import org.openrdf.model.Value;
-import org.openrdf.model.impl.LinkedHashModel;
-import org.openrdf.model.impl.URIImpl;
-import org.openrdf.repository.Repository;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFHandlerException;
 import org.openrdf.rio.Rio;
@@ -44,9 +39,7 @@ import org.restlet.resource.Post;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.HashSet;
-import java.util.Set;
 
 /**
  * @author Hannes Ebner
@@ -61,16 +54,20 @@ public class CacheResource extends BaseResource {
 
 	private Representation input;
 
+	private Cache cache;
+
 	@Get
 	public Representation represent() {
 		if (!hasAllParameters()) {
 			getResponse().setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
 			return null;
 		}
+		this.cache = getLDCache().getCache();
+
 		Representation output = null;
 		log.info("Received caching request for " + url);
 
-		Model resultGraph = mergeGraphs(new HashSet<Value>(Arrays.asList(url)), follow, new HashSet<URI>(), 0, depth);
+		Model resultGraph = cache.getMergedGraphs(new HashSet<Value>(Arrays.asList(url)), follow, new HashSet<URI>(), 0, depth);
 		if (resultGraph.size() > 0) {
 			String outputMediaType = getRequest().getClientInfo().getPreferredMediaType(RdfMedia.SUPPORTED_MEDIA_TYPES).getName();
 			log.debug("Writing content in format " + outputMediaType);
@@ -135,87 +132,9 @@ public class CacheResource extends BaseResource {
 
 		// FIXME start a thread to do the following
 
-		cacheResources(jsonArrayToSet(toAdd), jsonArrayToSet(toFollow), new HashSet<URI>(), 0, depth);
+		cache.loadAndCacheResources(JsonUtil.jsonArrayToSet(toAdd), JsonUtil.jsonArrayToSet(toFollow), new HashSet<URI>(), 0, depth);
 
 		getResponse().setStatus(Status.SUCCESS_OK); // FIXME change to ACCEPTED if run in background thread
-	}
-
-	private Set<Value> jsonArrayToSet(JSONArray array) {
-		Set<Value> result = new HashSet<Value>();
-		for (int i = 0; i < array.length(); i++) {
-			String uri = null;
-			try {
-				uri = array.getString(i);
-			} catch (JSONException e) {
-				log.warn(e.getMessage());
-				continue;
-			}
-			if (uri != null) {
-				result.add(new URIImpl(uri));
-			}
-		}
-		return result;
-	}
-
-	private void cacheResources(Set<Value> resources, Set<Value> propertiesToFollow, Set<URI> visited, int level, int depth) {
-		for (Value r : resources) {
-			if (!(r instanceof URI)) {
-				continue;
-			}
-			if (visited.contains((URI) r)) {
-				log.debug("Already visited, skipping: " + r);
-				continue;
-			}
-			visited.add((URI) r);
-			Model graph = HttpUtil.getModelFromRespone(HttpUtil.getResourceFromURL(r.toString(), 0));
-			if (graph != null) {
-				RdfResource res = new RdfResource((URI) r, graph, new Date());
-				RdfResource.saveToRepository(getLDCache().getRepository(), res);
-				log.info("Cached in local repository: " + r);
-				if (propertiesToFollow != null && level < depth+1) {
-					for (Value prop : propertiesToFollow) {
-						if (prop instanceof URI) {
-							log.debug("Following: " + prop);
-							Model matches = graph.filter((org.openrdf.model.Resource) null, (URI) prop, (Value) null);
-							cacheResources(matches.objects(), propertiesToFollow, visited, ++level, depth);
-						}
-					}
-				}
-			} else {
-				log.error("Model was null for: " + r.toString());
-			}
-		}
-	}
-
-	private Model mergeGraphs(Set<Value> resources, Set<Value> propertiesToFollow, Set<URI> visited, int level, int depth) {
-		Model result = new LinkedHashModel();
-		for (Value r : resources) {
-			if (!(r instanceof URI)) {
-				continue;
-			}
-			if (visited.contains((URI) r)) {
-				log.debug("Already visited, skipping: " + r);
-				continue;
-			}
-			visited.add((URI) r);
-			Resource res = RdfResource.loadFromRepository(getLDCache().getRepository(), (URI) r);
-			if (res == null || res.getGraph() == null) {
-				continue;
-			}
-			log.info("Loaded from local repository: " + r);
-			Model graph = res.getGraph();
-			result.addAll(graph);
-			if (propertiesToFollow != null && level < depth+1) {
-				for (Value prop : propertiesToFollow) {
-					if (prop instanceof URI) {
-						log.debug("Following: " + prop);
-						Model matches = graph.filter((org.openrdf.model.Resource) null, (URI) prop, (Value) null);
-						result.addAll(mergeGraphs(matches.objects(), propertiesToFollow, visited, ++level, depth));
-					}
-				}
-			}
-		}
-		return result;
 	}
 
 	private boolean hasAllParameters() {
